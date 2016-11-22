bb6427c8904a7fc7b6d6ef1fbd42b65fdb08661a
Dyluckk <zachary.wentworth@gmail.com>
2016-11-21 18:29:36 -0800

    init commit

---
+++ test/test.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var expect = require('chai').expect;
+var assert = require('assert');
+
+describe('MOCHA', function(){
+  it('should pass when mocha chai is okay', function(){
+      expect(true).to.be.true;
+  });
+});
