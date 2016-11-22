66821cfde4d2a792d80f1ea39ba13f68c380b5cb
Dyluckk <zachary.wentworth@gmail.com>
2016-11-21 18:47:26 -0800

    fixed this garbage

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel'] }
+      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot-loader/webpack', 'babel'] }
     ]
   }
 };
